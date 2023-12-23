1cbff1d2c0356d50f421ba105017d4eef3f1b769
fxliang <fx.liang@outlook.com>
2023-12-23 22:36:07 +0800

    add prepare env file

---
+++ prepare_env.bat
@@ -0,0 +1,4 @@
+@echo off
+copy /y python38._pth .\python-3.8.10-embed-win32\
+.\python-3.8.10-embed-win32\python.exe get-pip.py
+.\python-3.8.10-embed-win32\Script\pip.exe install -r requirement.txt
